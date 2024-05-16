f4315ea13a72e5000474d2a73d20ba7f38d22ba2
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-16 20:07:43 +0200

    add function to fetch data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+async function getWeatherData(location) {
+    const apiKey = "9YBLYGXYQRMR26C25MP2BV8SB";
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=metric&key=${apiKey}`;
+
+    const response = await fetch(url, { mode: "cors" });
+    const data = await response.json();
+
+    return data;
+}
+
+async function main() {
+    const weatherData = await getWeatherData("naturns");
+    console.log(weatherData);
+}
+
+main();
