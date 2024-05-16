0b0acd8868774213f68d9c157d3636fd7bd7a30a
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-16 23:42:55 +0200

    process weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import { getWeatherData } from "./lib/utils";
+import { getWeatherData, processData } from "./lib/utils";
 import "./style.css";
 
 async function main() {
     const weatherData = await getWeatherData("naturns");
+    const processed = processData(weatherData);
+
     console.log(weatherData);
+    console.log(processed);
 }
 
 main();
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,20 @@
 /**
+ * @param {number} value
+ * @returns {number}
+ */
+function celsiusToFahrenheit(value) {
+    return Number((value * (9 / 5) + 32).toFixed(1));
+}
+
+/**
+ * @param {number} value
+ * @returns {number}
+ */
+function kmhToMph(value) {
+    return Number((value * 0.621371).toFixed(1));
+}
+
+/**
  * @param {string} location
  */
 async function getWeatherData(location) {
@@ -11,4 +27,52 @@ async function getWeatherData(location) {
     return data;
 }
 
-export { getWeatherData };
+/**
+ * @param {object} data
+ * @returns {object}
+ */
+function processData(data) {
+    const dataConditions = data.currentConditions;
+    const currentConditions = {
+        conditions: dataConditions.conditions,
+        icon: dataConditions.icon,
+        humidity: dataConditions.humidity,
+        metric: {
+            temp: dataConditions.temp,
+            windspeed: dataConditions.windspeed,
+        },
+        us: {
+            temp: celsiusToFahrenheit(dataConditions.temp),
+            windspeed: kmhToMph(dataConditions.windspeed),
+        },
+    };
+
+    const days = [];
+    const dataDays = data.days;
+    dataDays.forEach((day) => {
+        days.push({
+            icon: day.icon,
+            datetime: day.datetime,
+            humidity: day.humidity,
+            metric: {
+                tempmin: day.tempmin,
+                tempmax: day.tempmax,
+                windspeed: day.windspeed,
+            },
+            us: {
+                tempmin: celsiusToFahrenheit(day.tempmin),
+                tempmax: celsiusToFahrenheit(day.tempmax),
+                windspeed: kmhToMph(day.windspeed),
+            },
+        });
+    });
+
+    return {
+        address: data.resolvedAddress,
+        description: data.description,
+        currentConditions,
+        days,
+    };
+}
+
+export { getWeatherData, processData };
