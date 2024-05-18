dd5dad4193b956e1b4db116ecd069be464bf7261
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-18 17:59:06 +0200

    formatting

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,12 @@ import { format } from "date-fns";
  */
 function header(title, description) {
     return `
-        <h2>${title} <span id="header-date">${format(Date.now(), "E, dd MMM yyyy")}</span></h2>
+        <h2>
+            ${title}
+            <span id="header-date">
+                ${format(Date.now(), "E, dd MMM yyyy")}
+            </span>
+        </h2>
         <p>${description}</p>
     `;
 }
